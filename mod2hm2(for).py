81eb4e0688cb9fc6b837269976081301d70f6091
Aleksandr <alex.kharchenko@mail.ru>
2024-05-27 18:31:38 +0300

    Add files via upload

--- mod2hm2(for).py
+++ mod2hm2(for).py
@@ -1,6 +1,17 @@
-cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-cars_count = 0
-for i in cars:
-    cars_count += 10
-    print("Я езжу на автомабиле марки " + i)
-    print(cars_count)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+no_primes = []
+is_prime = True
+
+for i in numbers:
+    is_prime = True
+    for j in range(2, i//2+1):
+        if i % j == 0:
+            is_prime = False
+            no_primes.append(i)
+            break
+    if is_prime:
+        primes.append(i)
+primes.remove(1)
+print('Primes: ', primes)
+print('No primes: ', no_primes)
